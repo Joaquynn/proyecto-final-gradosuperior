b29dc5cf2af56af928d964afc28e008c3b9c6ed2
Joaquín Ferrer Benzegga <40529220+Joaquynn@users.noreply.github.com>
2022-05-29 17:21:24 +0200

    Delete virtuaVoca_INSTALL directory

--- virtuaVoca_INSTALL/data/vv/db/api/userDelete.php
+++
@@ -1,5 +0,0 @@
-<?php
-include('../db_connection.php');
-$data = $_POST;
-$sql = "DELETE FROM `users` WHERE `users`.`username` = '".$data["username"]."'";
-$cx->exec( $sql );
--- virtuaVoca_INSTALL/data/vv/db/api/userUnban.php
+++
@@ -1,6 +0,0 @@
-<?php
-ini_set("display_errors", 1);
-include('../db_connection.php');
-$data = $_POST;
-$sql = "UPDATE `users` SET `status` = '1' WHERE `users`.`username` = '".$data["username"]."'";
-$cx->exec( $sql );
